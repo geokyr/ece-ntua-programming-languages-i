5c322d1d3a8f52f22459c831b72ee001ce3f0e06
geokyr <imgeorgekyr@gmail.com>
2021-04-19 22:19:00 +0300

    changed names

--- ex1/a/longest2.cpp
+++ ex1/a/longest2.cpp
@@ -1,76 +1,64 @@
 #include <iostream>
-#include <climits>
-#include <algorithm>
+#include <cmath>
 #include <fstream>
 using namespace std;
 
-// int* prefix(int arr[], int days) {
-//     int prefix[days + 1];
-//     prefix[0] = 0;
-    
-//     for (int i = 1; i < days; i++) {
-//         prefix[i] = prefix[i - 1] + arr[i - 1];
-//     }
+int search(int* searchspace, int s, int e, int key) {
+        int ans = -1;
 
-//     return prefix;
-// }
+        while(s <= e) {
+            int mid = (s + e) /2 ;
 
-int longestSubarray(int arr[], int len) {
-    int lcnt = 1, left[len] = {0};
-    
-    for(int i = 1; i < len; i++) {
-        if(arr[left[lcnt - 1]] > arr[i]) {
-            left[lcnt++] = i;
-        }
-    }
-
-    int rcnt = 1, right[len] = {len - 1};
-
-    for(int i = len - 1; i > 0; i--) {
-        if(arr[right[rcnt - 1]] < arr[i]) {
-            right[rcnt++] = i;
-        }
-    }
-
-    reverse(right, right + rcnt);
-
-    for(int i = 0; i < lcnt; i++) {
-        cout << left[i] << ", " << arr[left[i]] << endl;
-    }
-
-    for(int i = 0; i < rcnt; i++) {
-        cout << right[i] << ", " << arr[right[i]] << endl;
-    }
-
-    int answer = -1, leftp = 0, rightp = 0;
-
-    while(leftp < lcnt && rightp < rcnt) {
-        if(arr[right[rightp]] >= arr[left[leftp]]) {
-            while (rightp + 1 < rcnt && arr[right[rightp + 1]] >= arr[left[leftp]]) {
-                rightp++;
+            if(searchspace[mid] - key >= 0) {
+                ans = mid;
+                e = mid - 1;
             }
-            answer = max(answer, right[rightp] - left[leftp]);
-            leftp++;
-            rightp++;
-        } 
-        else if(left[leftp] < right[rightp] - 1){
-            leftp++;
+            else {
+                s = mid + 1;
+            }
         }
-        else {
-            rightp++; //leftp++ optimization
-        }
+        return ans;
+}
+    
+int longestSubarray(int a[], int n) {
+    int SuffixSum[n + 1];
+    SuffixSum[n] = 0;
+
+    for(int i = n - 1; i >= 0; --i) {
+        SuffixSum[i] = SuffixSum[i + 1] + a[i];
     }
-    return answer;
+
+    int ans = 0;
+    int searchspace[n];
+    int index[n];
+
+    int j = 0;
+    
+    for (int i = 0; i < n; ++i) { 
+
+        if (j == 0 or SuffixSum[i] > searchspace[j - 1]) { 
+            searchspace[j] = SuffixSum[i]; 
+            index[j] = i; 
+            j++; 
+        } 
+
+        int idx = search(searchspace, 0, j - 1, SuffixSum[i + 1]); 
+
+        if (idx != -1) 
+            ans = max(ans, i - index[idx] + 1); 
+    } 
+
+    return ans; 
 }
 
 int main(int argc, char *argv[]) {
-    // int days = 11;
-    // int hospitals = 3;
-    // int a[days] = {42, -10, 8, 1, 11, -6, -12, 16, -15, -11, 13};
-    
     // int days = 6;
     // int hospitals = 3;
-    // int a[6] = { 10, -4, -3 ,-2, 3, 8 };
+    // int arr[days] = {10, -4, -3 ,-3, 3, 8};
+
+    // int days = 11;
+    // int hospitals = 3;
+    // int arr[days] = {42, -10, 8, 1, 11, -6, -12, 16, -15, -11, 13};
 
     // -avg = -sum/cnt >= hospitals
     // -avg' = -sum'/cnt >= 0
@@ -88,33 +76,27 @@ int main(int argc, char *argv[]) {
     }
 
     int arr[days];
-
+    
     if(myfile.is_open()) {
         for(int i = 0; i < days; i++) {
             myfile >> arr[i];
-            // cout << "arr[" << i << "]: " << arr[i] << endl;
+            // cout << "arr[" << i << "]: " << arr[i] << endl;;
         }
     }
 
     myfile.close();
 
-    for(int i = 0; i < days; i++) {
+    for(int i = 0; i<days; i++) {
         arr[i] = -arr[i];
         arr[i] -= hospitals;
         // cout << arr[i] << ", ";
     }
-
-    int prefix[days + 1];
-    prefix[0] = 0;
-    
-    for (int i = 1; i < days + 1; i++) {
-        prefix[i] = prefix[i - 1] + arr[i - 1];
-        // cout << prefix[i] << ", ";
-    }
-
-    cout << longestSubarray(prefix, days + 1) << endl;
+    cout << longestSubarray(arr, days) << endl;
     return 0;
+
 }
 
-//https://cs.stackexchange.com/questions/129353/find-the-length-of-the-longest-subarray-having-sum-greater-than-k
-//https://replit.com/@Apass_Jack/cs129353longestsubarray
+// https://www.geeksforgeeks.org/longest-subarray-with-sum-greater-than-equal-to-zero/
+// https://leetcode.com/problems/longest-well-performing-interval/discuss/339791/c-on-solution-beats-9932-reduced-to-finding-longest-subarray-with-positive-sum
+// https://stackoverflow.com/questions/28356453/longest-positive-sum-substring
+// https://www.geeksforgeeks.org/largest-subarray-having-sum-greater-than-k/
