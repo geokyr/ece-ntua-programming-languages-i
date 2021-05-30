06af0a480e8a7ec08ee7c903668a8f6ff234ba4b
geokyr <imgeorgekyr@gmail.com>
2021-05-31 02:51:55 +0300

    tuples version of ex2/c

--- ex2/c/qssortold.py
+++ ex2/c/qssortold.py
@@ -12,25 +12,29 @@ def next(a):
     for i in range(2):
         if(i == 0 and a[0]):
             q = a[0][1:]
-            s = a[1][:] + (a[0][0],)
+            s = a[1][:] + [a[0][0]]
             p = a[2] + 'Q'
             yield(q, s, p)
         elif(i == 1 and a[1]):
             s = a[1][:-1]
-            q = a[0][:] + (a[1][-1],)
+            q = a[0][:] + [a[1][-1]]
             p = a[2] + 'S'
             yield(q, s, p)
 
 file = open(sys.argv[1], 'r')
 n = int(file.readline().strip())
-queue = tuple(list(map(int, file.readline().split())))
+queue = file.readline().split()
 file.close()
 
-stack = ()
+stack = []
 init = (queue, stack, '')
 
 Q = deque([init])
-visited = {hash((queue, stack))}
+hq = ' '.join(queue)
+hs = ' '.join(stack)
+hq = hash(hq)
+hs = hash(hs)
+visited = {(hq, hs)}
 solved = False
 
 while Q:
@@ -40,10 +44,13 @@ while Q:
         break
     else:
         for t in next(s):
-            h = hash((t[0], t[1]))
-            if(h not in visited):
+            hq = ' '.join(t[0])
+            hs = ' '.join(t[1])
+            hq = hash(hq)
+            hs = hash(hs)
+            if((hq, hs) not in visited):
                 Q.append(t)
-                visited.add(h)
+                visited.add((hq, hs))
 
 if solved:
     if(s[2]):
